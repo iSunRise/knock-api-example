e6aefe462b54bf86596caa25f018fce50bad7f00
Alexander D <alex.hh@me.com>
2016-11-29 17:58:03 +0200

    fix swagger docs generator

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,5 @@
 class Swagger::Docs::Config
+  def self.base_api_controller; ApplicationController end
   def self.transform_path(path, _api_version)
     # Make a distinction between the APIs and API documentation paths.
     "apidocs/#{path}"
@@ -10,6 +11,12 @@ Swagger::Docs::Config.register_apis(
     controller_base_path: '',
     api_file_path: 'public/apidocs',
     base_path: 'http://localhost:3000',
-    clean_directory: true
+    clean_directory: true,
+    api_extension_type: :json,
+    attributes: {
+      info: {
+        title: 'TechIdeation API'
+      }
+    }
   }
 )
