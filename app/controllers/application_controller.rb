15f9f6967793865d08527d203d8e38916470d4b0
Alexander D <alex.hh@me.com>
2017-02-13 20:37:51 +0200

    updated format of response with errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::API
 
   def render_errors(resource, errors = nil)
     errors ||= resource.errors
+    errors = errors.map do |key, message|
+      [key, key.to_s.humanize + ' ' + message]
+    end.to_h
     render json: { errors: errors }, status: 422
   end
 end
