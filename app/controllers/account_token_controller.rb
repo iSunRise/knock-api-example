de64318462b89132dabd09462181dc6a84f8aaf2
Alexander D <alex.hh@me.com>
2017-02-20 20:36:30 +0200

    return 401 if verification code failed

--- app/controllers/account_token_controller.rb
+++ app/controllers/account_token_controller.rb
@@ -10,6 +10,8 @@ class AccountTokenController < Knock::AuthTokenController
   def two_factor_auth
     if params[:auth][:phone_verification_code] == '1111'
       render json: { jwt: auth_token.token, two_factor_auth: false }, status: :created
+    else
+      render json: { error: 'Verification code is incorrect'}, status: 401
     end
   end
 end
